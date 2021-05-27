ea752ce895d69152674e10740dd24700da772130
Luciana Ferreira Baptista <luciana.baptista@etec.sp.gov.br>
2021-05-27 11:57:44 -0300

    Adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,51 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Coringa (2019)',
+      lancamento: '03/10/2019',
+      duracao: '2h 2m',
+      classificacao: 82,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/xLxgVxFWvb9hhUyCDDXxRPPnFck.jpg',
+      generos: ['Crime', 'Thriller', 'Drama']
+    },
+    {
+      nome: 'Tom & Jerry - O Filme (2021)',
+      lancamento: '11/02/2021',
+      duracao: '1h 41m',
+      classificacao: 73,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
+      generos: ['Comédia', 'Família', 'Animação']
+    },
+    {
+      nome: 'Convenção das Bruxas (2020)',
+      lancamento: '19/11/2020',
+      duracao: '1h 46m',
+      classificacao: 67,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/orrJiQs30G6zSkT8is4QOAtRpFM.jpg',
+      generos: ['Família', 'Fantasia', 'Aventura', 'Comédia', 'Terror']
+    },
+    {
+      nome: 'Twist (2021)',
+      lancamento: '22/01/2021',
+      duracao: '1h 31m',
+      classificacao: 65,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/h3oT6lZcYC2khtZnzHBgqthj5W.jpg',
+      generos: ['Crime', 'Drama', 'Ação']
+    },
+    {
+      nome: 'The Last - Naruto O Filme (2014)',
+      lancamento: '28/05/2015',
+      duracao: '1h 54m',
+      classificacao: 78,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/1lt7uwXxNXxurOQqnEyosG6Y5zb.jpg',
+      generos: ['Ação', 'Romance', 'Animação']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
