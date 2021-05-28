4672b31a869f64d0678c00924bc21a6c426be648
Luciana Ferreira Baptista <luciana.baptista@etec.sp.gov.br>
2021-05-27 22:50:11 -0300

    Serviço temporário e envio de dados do filme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'mortal-kombat',
+    loadChildren: () => import('./filmes/mortal-kombat/mortal-kombat.module').then( m => m.MortalKombatPageModule)
+  },
+  {
+    path: 'dados-filme',
+    loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
   }
 ];
 @NgModule({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,16 @@
+import { DadosService } from './../services/dados.service';
 import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   titulo = 'Vídeos';
 
   listaVideos: IFilme[] = [
@@ -18,44 +19,58 @@ export class Tab1Page {
       lancamento: '03/10/2019',
       duracao: '2h 2m',
       classificacao: 82,
-      cartaz: 'https://www.themoviedb.org/t/p/w1280/xLxgVxFWvb9hhUyCDDXxRPPnFck.jpg',
-      generos: ['Crime', 'Thriller', 'Drama']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w1280/xLxgVxFWvb9hhUyCDDXxRPPnFck.jpg',
+      generos: ['Crime', 'Thriller', 'Drama'],
     },
     {
       nome: 'Tom & Jerry - O Filme (2021)',
       lancamento: '11/02/2021',
       duracao: '1h 41m',
       classificacao: 73,
-      cartaz: 'https://www.themoviedb.org/t/p/w1280/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
-      generos: ['Comédia', 'Família', 'Animação']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w1280/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
+      generos: ['Comédia', 'Família', 'Animação'],
     },
     {
       nome: 'Convenção das Bruxas (2020)',
       lancamento: '19/11/2020',
       duracao: '1h 46m',
       classificacao: 67,
-      cartaz: 'https://www.themoviedb.org/t/p/w1280/orrJiQs30G6zSkT8is4QOAtRpFM.jpg',
-      generos: ['Família', 'Fantasia', 'Aventura', 'Comédia', 'Terror']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w1280/orrJiQs30G6zSkT8is4QOAtRpFM.jpg',
+      generos: ['Família', 'Fantasia', 'Aventura', 'Comédia', 'Terror'],
     },
     {
       nome: 'Twist (2021)',
       lancamento: '22/01/2021',
       duracao: '1h 31m',
       classificacao: 65,
-      cartaz: 'https://www.themoviedb.org/t/p/w1280/h3oT6lZcYC2khtZnzHBgqthj5W.jpg',
-      generos: ['Crime', 'Drama', 'Ação']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w1280/h3oT6lZcYC2khtZnzHBgqthj5W.jpg',
+      generos: ['Crime', 'Drama', 'Ação'],
     },
     {
       nome: 'The Last - Naruto O Filme (2014)',
       lancamento: '28/05/2015',
       duracao: '1h 54m',
       classificacao: 78,
-      cartaz: 'https://www.themoviedb.org/t/p/w1280/1lt7uwXxNXxurOQqnEyosG6Y5zb.jpg',
-      generos: ['Ação', 'Romance', 'Animação']
-    }
+      cartaz:
+        'https://www.themoviedb.org/t/p/w1280/1lt7uwXxNXxurOQqnEyosG6Y5zb.jpg',
+      generos: ['Ação', 'Romance', 'Animação'],
+    },
   ];
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public dadosService: DadosService,
+    public route: Router) {}
+
+  exibirFilme(filme: IFilme) {
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -67,14 +82,15 @@ export class Tab1Page {
           role: 'cancel',
           handler: (blah) => {
             console.log('Confirm Cancel: blah');
-          }
-        }, {
+          },
+        },
+        {
           text: 'SIM, favoritar',
           handler: () => {
             this.apresentarToast();
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -84,9 +100,8 @@ export class Tab1Page {
     const toast = await this.toastController.create({
       message: 'Filme adicionado aos favoritos.',
       duration: 2000,
-      color: 'success'
+      color: 'success',
     });
     toast.present();
   }
-
 }
