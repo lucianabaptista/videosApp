a878347fd424dcd23407243412f32e643f24561f
Luciana Ferreira Baptista <luciana.baptista@etec.sp.gov.br>
2021-06-02 22:45:58 -0300

    Adicionamos a API do The Movie DB e arrumamos toda a sua interface com os serviços e Models

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,22 @@
+import { GeneroService } from './../services/genero.service';
+import { IFilmeApi } from './../models/IFilmeAPI.model';
+import { FilmeService } from './../services/filme.service';
 import { DadosService } from './../services/dados.service';
 import { IFilme } from '../models/IFilme.model';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { IListaFilmes } from '../models/IFilmeAPI.model';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
-  titulo = 'Vídeos';
+export class Tab1Page implements OnInit {
+
+  titulo = 'Filmes';
 
   listaVideos: IFilme[] = [
     {
@@ -61,13 +66,30 @@ export class Tab1Page {
     },
   ];
 
+  listaFilmes: IListaFilmes;
+
+  generos: string[] = [];
+
   constructor(
     public alertController: AlertController,
     public toastController: ToastController,
     public dadosService: DadosService,
+    public filmeService: FilmeService,
+    public generoService: GeneroService,
     public route: Router) {}
 
-  exibirFilme(filme: IFilme) {
+  buscarFilmes(evento: any){
+    console.log(evento.target.value);
+    const busca = evento.target.value;
+    if (busca && busca.trim() !== '') {
+      this.filmeService.buscarFilmes(busca).subscribe(dados => {
+        console.log(dados);
+        this.listaFilmes = dados;
+      });
+    }
+  }
+
+  exibirFilme(filme: IFilmeApi) {
     this.dadosService.guardarDados('filme', filme);
     this.route.navigateByUrl('/dados-filme');
   }
@@ -103,5 +125,16 @@ export class Tab1Page {
       color: 'success',
     });
     toast.present();
+  }
+
+  ngOnInit(){
+    this.generoService.buscarGeneros().subscribe(dados =>{
+      console.log('Generos: ', dados.genres);
+      dados.genres.forEach(genero => {
+        this.generos[genero.id] = genero.name;
+      });
+
+      this.dadosService.guardarDados('generos', this.generos);
+    });
   }
 }
